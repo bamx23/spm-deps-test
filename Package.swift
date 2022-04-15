eb8ed85c4649dd5a2d7f9979fc67bea17069dd67
Nikolay Volosatov <mikalai.valasatau@mapbox.com>
2022-04-15 16:29:50 +0300

    Try different declarations

--- Package.swift
+++ Package.swift
@@ -7,8 +7,31 @@ let package = Package(
     name: "SPMDepsTest",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/bamx23/spm-deps-library-a.git", from: "1.1.0-rc.1"),
-        .package(url: "https://github.com/bamx23/spm-deps-library-b.git", from: "10.0.0"),
+
+//         This works:
+//         .package(url: "https://github.com/bamx23/spm-deps-library-a.git", from: "1.1.0-rc.1"),
+
+//         This works too:
+//        .package(url: "https://github.com/bamx23/spm-deps-library-a.git", .branch("main")),
+
+//         And this works:
+//         .package(url: "https://github.com/bamx23/spm-deps-library-a.git", .revision("b0c6c32")),
+
+//         Even this works:
+        .package(
+            url: "https://github.com/bamx23/spm-deps-library-a.git",
+            .init(
+                uncheckedBounds: (
+                    .init(stringLiteral: "1.1.0-rc.1"),
+                    .init(stringLiteral: "1.1.0-rc.1")
+                )
+            ) as ClosedRange
+        ),
+
+//         But this can't be resolved:
+//         .package(url: "https://github.com/bamx23/spm-deps-library-a.git", .exact("1.1.0-rc.1")),
+
+         .package(url: "https://github.com/bamx23/spm-deps-library-b.git", from: "10.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
